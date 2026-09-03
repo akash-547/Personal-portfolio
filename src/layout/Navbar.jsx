3885925bd7770d1845824bf69748779e80338abe
akash-547 <kashhussain512@gmail.com>
2026-09-03 18:43:50 +0500

    fix: some name

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -34,7 +34,7 @@ export const Navbar = () => {
           href="#"
           className="text-xl font-bold tracking-tight hover:text-primary"
         >
-          PM<span className="text-primary">.</span>
+          Portfolio<span className="text-primary">.</span>
         </a>
 
         {/* Desktop Nav */}
